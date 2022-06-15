18fe1216bda731408d800ebf7d13ea4149749c03
rafaelalmeida9 <82919665+rafaelalmeida9@users.noreply.github.com>
2022-06-15 09:12:26 -0300

    Mais endpoints

--- server/app.js
+++ server/app.js
@@ -9,7 +9,7 @@ require('dotenv').config()
 const port = process.env.PORT
 
 const app = express()
-// Tipos de dados
+
 app.use(express.json()); 
 app.use(morgan('dev'))
 app.use(express.urlencoded({ extended: false }))
@@ -46,49 +46,33 @@ app.get('/users/:id', async function (req, res) {
 })
 
 app.post('/cards', async function (req, res) {
-    //Variaveis para guardar os parametros passados pelo frontend
     const per = req.body.pergunta
     const resp = req.body.resposta
     const respCerta = req.body.respostaCerta
 
-    //TO DO: validar os campos --------------------
-
-    //Cria model com as variaveis e coloca no banco de dados
     const elemento = await cardModel.create({
         pergunta: per, 
         resposta: resp,
         respostaCerta: respCerta
     });
-    //Retorno de resposta
+
     return res.json(elemento)
 })
 
 app.post('/decks', async function (req, res) {
-    //Variaveis para guardar os parametros passados pelo frontend
     const cards = req.body.cards
     const score = req.body.score
 
-    //TO DO: validar os campos --------------------
-
-    // try {
-    //     if(JSON.stringify(cards) == "[]") throw "Deck Vazio";
-    // }
-    // catch(err) {
-    //     console.log("Erro: "+err)
-    //     return;
-    // }
-
-    //Cria model com as variaveis e coloca no banco de dados
     const elemento = await deckModel.create({
         cards,
         score
     });
-    //Retorno de resposta
+
     return res.json(elemento)
 })
 
 app.post('/users', async function (req, res) {
-    //Variaveis para guardar os parametros passados pelo frontend
+
     const username = req.body.username
     const password = req.body.password
     const decks = req.body.decks
@@ -98,11 +82,11 @@ app.post('/users', async function (req, res) {
         password,
         decks
     });
-    //Retorno de resposta
+
     return res.json(elemento)
 })
 
-app.put('/addCard/:idDeck/:idCard', async (req, res) => {
+app.put('/deckAddCard/:idDeck/:idCard', async (req, res) => {
     const idDeck = req.params.idDeck
     const idCard = req.params.idCard
   
@@ -114,13 +98,37 @@ app.put('/addCard/:idDeck/:idCard', async (req, res) => {
     return res.json(modify)
 })
 
-app.put('/addDeck/:idUser/:idDeck', async (req, res) => {
+app.put('/userAddDeck/:idUser/:idDeck', async (req, res) => {
     const idUser = req.params.idUser
     const idDeck = req.params.idDeck
 
     const modify = await userModel.findOneAndUpdate(
         {_id : idUser},
         {$push: { decks: idDeck }},
+        {new: true})
+
+    return res.json(modify)
+})
+
+app.put('/deckRemoveCard/:idDeck/:idCard', async (req, res) => {
+    const idDeck = req.params.idDeck
+    const idCard = req.params.idCard
+  
+    const modify = await deckModel.findOneAndUpdate(
+        {_id : idDeck},
+        {$pull: { cards: idCard }},
+        {new: true})
+  
+    return res.json(modify)
+})
+
+app.put('/userRemoveDeck/:idUser/:idDeck', async (req, res) => {
+    const idUser = req.params.idUser
+    const idDeck = req.params.idDeck
+
+    const modify = await userModel.findOneAndUpdate(
+        {_id : idUser},
+        {$pull: { decks: idDeck }},
         {new: true})
 
     return res.json(modify)
@@ -154,12 +162,12 @@ app.delete('/decks/:id', async (req, res) => {
     return res.json(deleteOne)
 })
 
-//Chama a funcao main para conectar ao banco de dados
 main().catch(err => console.log(err));
-//Chama o mongoose para conectar no banco
+
 async function main() {
-  await mongoose.connect(process.env.DB_URL);
-  app.listen(port, () => {
-    console.log(`Servidor aberto em: http://localhost:${port}`)
-  })
+    await mongoose.connect(process.env.DB_URL);
+
+    app.listen(port, () => {
+        console.log(`Servidor aberto em: http://localhost:${port}`);
+    })
 }
